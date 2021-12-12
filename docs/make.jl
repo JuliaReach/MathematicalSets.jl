ff4bbdd35e800543f9ec108e64a2ff4468475c99
schillic <git@christianschilling.net>
2021-12-12 17:11:42 +0100

    simpler style file + activate docs previews

--- docs/make.jl
+++ docs/make.jl
@@ -1,23 +1,25 @@
 using Documenter, MathematicalSets
 
+DocMeta.setdocmeta!(MathematicalSets, :DocTestSetup,
+                    :(using MathematicalSets); recursive=true)
+
 makedocs(;
     sitename="MathematicalSets.jl",
     modules=[MathematicalSets],
     format = Documenter.HTML(
         prettyurls = get(ENV, "CI", nothing) == "true",
-        assets = ["assets/juliareach.css"]),
+        assets = ["assets/aligned.css"]),
+    strict = true,
     pages = [
         "Home" => "index.md",
         "Library" => Any[
-        "Types" => "lib/types.md",
-        "Methods" => "lib/methods.md"],
+            "Types" => "lib/types.md",
+            "Methods" => "lib/methods.md"],
         "About" => "about.md"
-    ],
-    repo="https://github.com/JuliaReach/MathematicalSets.jl/blob/{commit}{path}#L{line}",
-    doctest = true,
-    strict = true
+    ]
 )
 
 deploydocs(;
-    repo="github.com/JuliaReach/MathematicalSets.jl"
+    repo = "github.com/JuliaReach/MathematicalSets.jl.git",
+    push_preview = true
 )
