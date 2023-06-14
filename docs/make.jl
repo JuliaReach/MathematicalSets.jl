ff582be8a144c440eafa4a4746bf7a6c592b2ed9
schillic <git@christianschilling.net>
2023-06-14 06:22:52 +0200

    format docs/make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -4,22 +4,16 @@ DocMeta.setdocmeta!(MathematicalSets, :DocTestSetup,
                     :(using MathematicalSets); recursive=true)
 
 makedocs(;
-    sitename="MathematicalSets.jl",
-    modules=[MathematicalSets],
-    format = Documenter.HTML(
-        prettyurls = get(ENV, "CI", nothing) == "true",
-        assets = ["assets/aligned.css"]),
-    strict = true,
-    pages = [
-        "Home" => "index.md",
-        "Library" => Any[
-            "Types" => "lib/types.md",
-            "Methods" => "lib/methods.md"],
-        "About" => "about.md"
-    ]
-)
+         sitename = "MathematicalSets.jl",
+         modules  = [MathematicalSets],
+         format   = Documenter.HTML(; prettyurls=get(ENV, "CI", nothing) == "true",
+         assets=["assets/aligned.css"]),
+         strict   = true,
+         pages    = ["Home" => "index.md",
+         "Library" => Any["Types" => "lib/types.md",
+         "Methods" => "lib/methods.md"],
+         "About" => "about.md"])
 
 deploydocs(;
-    repo = "github.com/JuliaReach/MathematicalSets.jl.git",
-    push_preview = true
-)
+           repo="github.com/JuliaReach/MathematicalSets.jl.git",
+           push_preview=true)
